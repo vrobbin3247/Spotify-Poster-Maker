05f038333df4cf995f8babdc4a0cc9b8d18adf17
vrobbin3247 <vaibhavgame3247@gmail.com>
2025-03-08 19:53:20 +0530

    added download function

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from poster_maker import generate_spotify_poster
 from spotify_caller import get_track_data
 import re
+from io import BytesIO
+
 
 # Regex pattern for Spotify track URLs
 SPOTIFY_TRACK_PATTERN = r"https?://open\.spotify\.com/track/[a-zA-Z0-9]+"
@@ -26,5 +28,28 @@ if url:
                 # Generate and display the poster
                 image = generate_spotify_poster(data)
                 st.image(image)
+                img_buffer = BytesIO()
+                image.save(img_buffer, format="png")
+                img_bytes = img_buffer.getvalue()
+
+                # st.markdown(
+                #     """
+                #     <style>
+                #     .stDownloadButton button {
+                #         background-color: #FF5733 !important; /* Custom color */
+                #     }
+                #     </style>
+                #     """,
+                #     unsafe_allow_html=True,
+                # )
+
+                st.download_button(
+                    label="Download Poster",
+                    data=img_bytes,
+                    file_name="poster.png",
+                    mime="image/jpeg",
+                    icon=":material/download:",
+                )
+
         except Exception as e:
             st.error(f"❌ Error fetching track data: {str(e)}")
